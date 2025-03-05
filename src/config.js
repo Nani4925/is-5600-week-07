5d3b90edac7b22ed6278231c2160f01cfeee270d
Venkata Brahmaji <venkata4925@gmail.com>
2025-03-05 04:17:30 +0000

    IS_5600_Week7_Lab07

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // TODO: Replace this with your own Server URL for your codespace
-export const BASE_URL = "https://glowing-space-memory-q7p4jvr9g4p424rj7-3080.app.github.dev";
+export const BASE_URL = "https://animated-doodle-r4p7j5wv5ggxh5wpr-3080.app.github.dev/";
